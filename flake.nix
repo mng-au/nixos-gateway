1aef07e8f73bf104a8f43e62bfdc7dfcc4731ac4
Matthew Ng <ng2matthew@gmail.com>
2026-07-03 16:51:57 +1000

    feat: add gateway-2 configs and vars

--- flake.nix
+++ flake.nix
@@ -18,7 +18,39 @@
       modules = [
         sops-nix.nixosModules.sops
         ./hosts/gateway-2/configuration.nix
+        ./modules/common.nix
+        ./modules/btrfs.nix
+        ./modules/gateway.nix
+        ./modules/adguardhome_custom.nix
+        ./modules/adguardhome.nix
+        ./modules/kea.nix
+        ./modules/fail2ban.nix
+        ./modules/netdata.nix
+        ./modules/danted.nix
+        ./modules/squid.nix
       ];
+
+      specialArgs = {
+        hostVars = {
+            inetIngressSpeedInMB = "500";
+            inetEngressSeeedInMB = "50";
+
+            ifInternet = "enp3s0f0";
+            ifLan1 = "enp3s0f1";
+            ifLan2 = "enp4s0f0";
+
+            ipDesktop1 = "192.168.11.36";
+            ipNginx = "192.168.11.23";
+            ipLan1 = "192.168.1.1";          # home
+            ipLan1Cidr = "192.168.1.0/24";
+            ipLan1DhcpRange = "192.168.1.100 - 192.168.1.200";
+            ipLan2 = "192.168.9.1";          # homelab
+            ipLan2Cidr = "192.168.8.0/22";
+            ipVpnProxy1 = "192.168.11.230";
+            ipServer1 = "192.168.11.245";
+            ipServer2 = "192.168.11.246";
+        };
+      };
     };
   };
 }
