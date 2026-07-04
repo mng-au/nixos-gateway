c06d6c0e12a341696de88cb10f4c97728586db3b
Matthew Ng <ng2matthew@gmail.com>
2026-07-04 19:00:08 +1000

    feat: add nginx and acme modules

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -1,14 +1,22 @@
-{ config, pkgs, sops, hostVars, ... }:
+{ config, pkgs, sops, hostVars,  ... }:
 
 let
   acme_domain = builtins.getEnv "ACME_DOMAIN";
   acme_default_email = builtins.getEnv "ACME_DEFAULT_EMAIL";
 in
 {
+  users.groups.acme = {};
+  users.users.acme = {
+   isSystemUser = true;
+   group = "acme";
+  };
+
+  sops.secrets."vars/cloudflare_dns_api_key" = {}; # Must declare the secret first
   sops.templates."cloudflare.env" = {
     content = ''
 CLOUDFLARE_DNS_API_TOKEN='${config.sops.placeholder."vars/cloudflare_dns_api_key"}'
     '';
+    owner = "acme";
   };
 
   security.acme = {
